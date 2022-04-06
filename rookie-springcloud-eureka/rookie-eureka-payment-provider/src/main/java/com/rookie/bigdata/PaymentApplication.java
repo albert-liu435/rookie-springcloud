ee1665c35ac8fd38bedbc123b40aea104bf5ffb7
liuxili01 <liuxili@bestseller.com.cn>
2022-04-06 18:13:14 +0800

    添加eureka

--- rookie-springcloud-eureka/rookie-eureka-payment-provider/src/main/java/com/rookie/bigdata/PaymentApplication.java
+++ rookie-springcloud-eureka/rookie-eureka-payment-provider/src/main/java/com/rookie/bigdata/PaymentApplication.java
@@ -2,6 +2,7 @@ package com.rookie.bigdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Classname PaymentApplication
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication {
 
     public static void main(String[] args) {
